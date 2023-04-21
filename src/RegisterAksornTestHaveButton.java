865a9c3fc8cd542d7b040d26565db8f67837f1f9
dreamNawaporn <96407096+dreamNawaporn@users.noreply.github.com>
2023-04-21 23:20:16 +0700

    Update RegisterAksornTestHaveButton.java

--- src/RegisterAksornTestHaveButton.java
+++ src/RegisterAksornTestHaveButton.java
@@ -30,29 +30,29 @@ public class RegisterAksornTestHaveButton {
 		driver.findElement(By.xpath("//span[contains(text(), 'คำนำหน้าชื่อ')]")).click();
 
 		WebElement titleSelect = driver.findElement(By.xpath("//input[@class='select2-search__field']"));
-		titleSelect.sendKeys("นาย");
+		titleSelect.sendKeys("นางสาว");
 		titleSelect.sendKeys(Keys.ENTER);
 
-		driver.findElement(By.name("first_name")).sendKeys("วรปกร");
+		driver.findElement(By.name("first_name")).sendKeys("นวพร");
 
-		driver.findElement(By.name("last_name")).sendKeys("จารุศิริพจน์");
+		driver.findElement(By.name("last_name")).sendKeys("บุญก่อน");
 
-		new Select(driver.findElement(By.xpath("//*[@id=\"gender\"]"))).selectByValue("M");
+		new Select(driver.findElement(By.xpath("//*[@id=\"gender\"]"))).selectByValue("N");
 
 		driver.findElement(By.xpath("//*[@id=\"select2-birth_year-container\"]")).click();
 
 		WebElement birthYearSelect = driver.findElement(By.xpath("//input[@class='select2-search__field']"));
-		birthYearSelect.sendKeys("2546");
+		birthYearSelect.sendKeys("2545");
 		birthYearSelect.sendKeys(Keys.ENTER);
 
-		driver.findElement(By.name("email")).sendKeys("644259018@webmail.npru.ac.th");
+		driver.findElement(By.name("email")).sendKeys("644259029@webmail.npru.ac.th");
 
-		driver.findElement(By.name("mobile_no")).sendKeys("0623844415");
+		driver.findElement(By.name("mobile_no")).sendKeys("0928811045");
 
-		driver.findElement(By.name("password")).sendKeys("a123456789"); // Replace with the decrypted
+		driver.findElement(By.name("password")).sendKeys("Dream4183113"); // Replace with the decrypted
 																		// password
 
-		driver.findElement(By.name("password_confirmation")).sendKeys("a123456789"); // Replace
+		driver.findElement(By.name("password_confirmation")).sendKeys("Dream4183113"); // Replace
 																						// with
 																						// the
 																						// decrypted
@@ -64,7 +64,7 @@ public class RegisterAksornTestHaveButton {
 		driver.findElement(By.xpath("//*[@id=\"select2-school_id-container\"]/span")).click();
 
 		WebElement schoolSelect = driver.findElement(By.xpath("/html/body/span/span/span[1]/input"));
-		schoolSelect.sendKeys("สิรินธรราชวิทยาลัย");
+		schoolSelect.sendKeys("วิทยาลัยเทคโนโลยีบริหารธุรกิจกาญจนบุรี");
 		Thread.sleep(1000);
 		schoolSelect.sendKeys(Keys.ENTER);
 
